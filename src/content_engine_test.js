4cf50213e69456ca9979a2671d9e69580f0dca92
FranciscoPonceGomez <pakopongo@gmail.com>
2019-08-21 15:03:49 -0700

    Semantically correct but boring

--- src/content_engine_test.js
+++ src/content_engine_test.js
@@ -2,22 +2,16 @@ const spacyNLP = require("spacy-nlp");
 const natural = require('natural')
 
 // vocabulary
-const human_touch_begin = ['Wow that was crazy! Do you think', 'Did it again!'];
+const human_touch = ['Wow that was crazy! Do you think', 'Did it again!'];
 const subject = ['streamer', 'player'];
-// const elimination = ['will eliminate', 'will be eliminated', 'will rekt'];
-// const survival = ['will make it', 'will survive'];
-// const position = ['will win this game', 'will lose this game'];
-// const action = [elimination, position, survival];
 const numbers = [1,2,3,4,5,6,7,8,9];
 const time_variable = [numbers.map(x => 'in the next ' + x + ' minutes'), 'before end of game'];
 const rank_variable = ['top 50', 'top 25', 'top 10', 'top 5'];
-const count_variable = numbers.map(x => x + ' players');
-const variables = [time_variable, rank_variable, count_variable];
-const human_touch_end = ['Wow', 'crazy', 'again', 'for real'];
-const filler = ['before', 'in the next'];
+const count_variable = [numbers.map(x => x + ' players'), numbers.map(x => x + ' times')];
+const filler = ['this game', 'this match', 'this session'];
 
 let elimination = {
-    "text": ['will eliminate', 'will be eliminated', 'will rekt'],
+    "text": ['will eliminate', 'will rekt'],
     "options": [
         [time_variable],
         [count_variable, time_variable],
@@ -25,19 +19,30 @@ let elimination = {
     ]
 };
 
+let eliminated = {
+    "text": ['will be eliminated', 'will get rekt'],
+    "options": [
+        [time_variable],
+        [time_variable, filler]
+    ]
+};
+
 let position = {
     "text": ['will win this game', 'will lose this game'],
     "options": [
-        [time_variable],
+        [time_variable]
     ]
 };
 
 let survival = {
-    "text": ['will make it to', 'will survive to'],
-    "options": [rank_variable]
+    "text": ['will make it to', 'will survive to', 'will place'],
+    "options": [
+        [rank_variable],
+    ]
 };
 
-let challenge_options = [elimination, survival, position];
+let challenge_options = [elimination, eliminated, survival, position];
+// let challenge_options = [elimination];
 let conversation_pipeline = [subject, challenge_options];
 
 const NGrams = natural.NGrams;
@@ -67,6 +72,5 @@ while(conversation_pipeline.length > 0) {
         conversation_pipeline.unshift(randomizer(el));
     }
 }
-res += "?"
 
 console.log(res);
