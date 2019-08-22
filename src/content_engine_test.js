d6840f5437911392a78ba97b5bbb0a108cd2241a
FranciscoPonceGomez <pakopongo@gmail.com>
2019-08-21 17:19:54 -0700

    Added spicy comments

--- src/content_engine_test.js
+++ src/content_engine_test.js
@@ -2,47 +2,49 @@ const spacyNLP = require("spacy-nlp");
 const natural = require('natural')
 
 // vocabulary
-const human_touch = ['Wow that was crazy! Do you think', 'Did it again!'];
-const subject = ['streamer', 'player'];
+const spice_start = ['Besides all the effort', 'Wow that was crazy! Do you think', 'Did it again!', 'She is really good!'];
+const spicy_end = ['and walk away', 'to consolidate his dominance', 'and call it a day', 'because he is up to no good'];
 const numbers = [2,3,4,5,6,7,8,9];
-const time_variable = [numbers.map(x => 'in the next ' + x + ' minutes'), 'before end of game'];
-const rank_variable = ['top 50', 'top 25', 'top 10', 'top 5'];
-const count_variable = [numbers.map(x => x + ' players'), numbers.map(x => x + ' times')];
+const time = [numbers.map(x => 'in the next ' + x + ' minutes'), 'before the end of game'];
+const rank = ['top 50', 'top 25', 'top 10', 'top 5', 'top 3'];
+const count = [numbers.map(x => x + ' players'), numbers.map(x => x + ' times')];
 const filler = ['this game', 'this match', 'this session'];
 
+// semantic relationship
 let elimination = {
-    "text": ['will eliminate', 'will rekt'],
+    "text": ['will eliminate', 'will rekt', 'will dominate', 'will own', 'will obliterate', 'will kill'],
     "options": [
-        [time_variable],
-        [count_variable, time_variable],
-        [count_variable]
+        [time],
+        [time, count],
+        [count],
+        [spicy_end, time, count]
     ]
 };
 
 let death = {
-    "text": ['will be eliminated', 'will get rekt'],
+    "text": ['will be eliminated', 'will get rekt', 'will be blasted', 'will be killed'],
     "options": [
-        [time_variable],
-        [time_variable, filler]
+        [time]
     ]
 };
 
 let position = {
-    "text": ['will win this game', 'will lose this game'],
+    "text": ['will win', 'will lose', 'will get a victory royale'],
     "options": [
-        [time_variable]
+        [filler],
+        [spicy_end, filler]
     ]
 };
 
 let survival = {
     "text": ['will make it to', 'will survive to', 'will place'],
     "options": [
-        [rank_variable],
+        [rank],
     ]
 };
 
 let start = {
-    "text": ['streamer', 'player'],
+    "text": ['streamer', 'Ninja', 'Shroud'],
     "options": [
         [elimination],
         [death],
@@ -70,11 +72,12 @@ function isDict(v) {
 let res = "";
 while(conversation_pipeline.length > 0) {
     el = conversation_pipeline.shift();  // remove first element
-    // console.log(conversation_pipeline);
     if(isDict(el)) {
         res = res + " " + randomizer(el.text);
         candidate = randomizer(el.options);
-        conversation_pipeline.unshift(candidate);  // add to the end of the array
+        for(e of candidate) {
+            conversation_pipeline.unshift(e);  // add to the end of the array
+        }
     }
     else if(typeof el === 'string' || el instanceof String) {
         res = res + " " + el;
@@ -82,6 +85,6 @@ while(conversation_pipeline.length > 0) {
     else {
         conversation_pipeline.unshift(randomizer(el));
     }
+    // console.log(conversation_pipeline);
 }
-
 console.log(res);
