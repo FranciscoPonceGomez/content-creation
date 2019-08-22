5daab7c646c33ec94bb7119196cd08d0ecf0b1a2
FranciscoPonceGomez <pakopongo@gmail.com>
2019-08-22 12:56:19 -0700

    Moved intro text from semantic to vocab

--- src/content_engine_test.js
+++ src/content_engine_test.js
@@ -4,6 +4,11 @@ const natural = require('natural')
 // vocabulary
 const spice_start = ['Besides all the effort', 'Wow that was crazy! Do you think', 'Did it again!', 'She is really good!'];
 const spicy_end = ['and walk away', 'to consolidate his dominance', 'and call it a day', 'because he is up to no good'];
+const elimination_intro = ['will eliminate', 'will rekt', 'will dominate', 'will own', 'will obliterate', 'will kill'];
+const death_intro = ['will be eliminated', 'will get rekt', 'will be blasted', 'will be killed'];
+const position_intro = ['will win', 'will lose', 'will get a victory royale'];
+const survival_intro = ['will make it to', 'will survive to', 'will place'];
+const start_intro = ['streamer', 'Ninja', 'Shroud'];
 const numbers = [2,3,4,5,6,7,8,9];
 const time = [numbers.map(x => 'in the next ' + x + ' minutes'), 'before the end of game'];
 const rank = ['top 50', 'top 25', 'top 10', 'top 5', 'top 3'];
@@ -12,7 +17,7 @@ const filler = ['this game', 'this match', 'this session'];
 
 // semantic relationship
 let elimination = {
-    "text": ['will eliminate', 'will rekt', 'will dominate', 'will own', 'will obliterate', 'will kill'],
+    "text": elimination_intro,
     "options": [
         [time],
         [time, count],
@@ -22,14 +27,14 @@ let elimination = {
 };
 
 let death = {
-    "text": ['will be eliminated', 'will get rekt', 'will be blasted', 'will be killed'],
+    "text": death_intro,
     "options": [
         [time]
     ]
 };
 
 let position = {
-    "text": ['will win', 'will lose', 'will get a victory royale'],
+    "text": position_intro,
     "options": [
         [filler],
         [spicy_end, filler]
@@ -37,14 +42,14 @@ let position = {
 };
 
 let survival = {
-    "text": ['will make it to', 'will survive to', 'will place'],
+    "text": survival_intro,
     "options": [
         [rank],
     ]
 };
 
 let start = {
-    "text": ['streamer', 'Ninja', 'Shroud'],
+    "text": start_intro,
     "options": [
         [elimination],
         [death],
