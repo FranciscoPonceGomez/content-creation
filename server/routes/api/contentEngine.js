afc4da5334132e44ec33deec79b03b086304693a
FranciscoPonceGomez <pakopongo@gmail.com>
2019-09-03 15:40:31 -0700

    missing demo bits

--- server/routes/api/contentEngine.js
+++ server/routes/api/contentEngine.js
@@ -2,67 +2,133 @@ const spacyNLP = require("spacy-nlp");
 const natural = require('natural')
 
 // vocabulary
-const spicy_start = ['Besides all the effort', 'Wow that was crazy! Do you think', 'Did it again!', 'She is really good!'];
-const spicy_end = ['and walk away', 'to consolidate his dominance', 'and call it a day', 'because he is up to no good'];
-const elimination_intro = ['will eliminate', 'will rekt', 'will dominate', 'will own', 'will obliterate', 'will kill'];
-const death_intro = ['will be eliminated', 'will get rekt', 'will be blasted', 'will be killed'];
-const position_intro = ['will win', 'will lose', 'will get a victory royale'];
-const survival_intro = ['will make it to', 'will survive to', 'will place'];
-const start_intro = ['Ninja'];
-const numbers = [2,3,4,5,6,7,8,9];
-const time = [numbers.map(x => 'in the next ' + x + ' minutes'), 'before the end of game'];
-const rank = ['top 50', 'top 25', 'top 10', 'top 5', 'top 3'];
-const count = [numbers.map(x => x + ' players'), numbers.map(x => x + ' times')];
-const filler = ['this game', 'this match', 'this session'];
+let spicy_start = ['Besides all the effort', 'Wow that was crazy! Do you think', 'Did it again!', 'She is really good!'];
+let spicy_end = ['and walk away', 'to consolidate his dominance', 'and call it a day', 'because he is up to no good'];
+let elimination_intro = ['will eliminate', 'will rekt', 'will dominate', 'will own', 'will obliterate', 'will kill'];
+let death_intro = ['will be eliminated', 'will get rekt', 'will be blasted', 'will be killed'];
+let position_intro = ['will win', 'will lose', 'will get a victory royale'];
+let survival_intro = ['will make it to', 'will survive to', 'will place'];
+let start_intro = ['Ninja'];
+let numbers = [2,3,4,5,6,7,8,9];
+let time = [numbers.map(x => 'in the next ' + x + ' minutes'), 'before the end of game'];
+let rank = ['top 50', 'top 25', 'top 10', 'top 5', 'top 3'];
+let filler = ['this game', 'this match', 'this session'];
+let count = [numbers.map(x => x + ' players'), numbers.map(x => x + ' times')];
+
+    // semantic relationship
+    let elimination = {
+        "type": "elimination",
+        "text": elimination_intro,
+        "options": [
+            [time],
+            [time, count],
+            [count],
+            [spicy_end, time, count]
+        ]
+    };
+
+    let death = {
+        "type": "death",
+        "text": death_intro,
+        "options": [
+            [time]
+        ]
+    };
+
+    let position = {
+        "type": "posisition",
+        "text": position_intro,
+        "options": [
+            [filler],
+            [spicy_end, filler]
+        ]
+    };
+
+    let survival = {
+        "type": "survival",
+        "text": survival_intro,
+        "options": [
+            [rank],
+        ]
+    };
+
+    let start = {
+        "text": start_intro,
+        "options": [
+            [elimination],
+            [death],
+            [survival],
+            [position]
+        ]
+    };
+
+function generateVocab (survivors_filter) {
+
+    numbers = [2,3,4,5,6,7,8,9].map(x => x < survivors_filter);
+    if (survivors_filter < 3) {
+        rank = ['top 1'];
+    }
+    if (survivors_filter < 5) {
+        rank = ['top 50', 'top 25', 'top 10', 'top 5', 'top 3'].splice(3,4);
+    }
+    if (survivors_filter < 10) {
+        rank = ['top 50', 'top 25', 'top 10', 'top 5', 'top 3'].splice(2,4);
+    }
+    if (survivors_filter < 25) {
+        rank = ['top 50', 'top 25', 'top 10', 'top 5', 'top 3'].splice(1,4);
+    }
+
+    // semantic relationship
+    elimination = {
+        "type": "elimination",
+        "text": elimination_intro,
+        "options": [
+            [time],
+            [time, count],
+            [count],
+            [spicy_end, time, count]
+        ]
+    };
+
+    death = {
+        "type": "death",
+        "text": death_intro,
+        "options": [
+            [time]
+        ]
+    };
+
+    position = {
+        "type": "posisition",
+        "text": position_intro,
+        "options": [
+            [filler],
+            [spicy_end, filler]
+        ]
+    };
+
+    survival = {
+        "type": "survival",
+        "text": survival_intro,
+        "options": [
+            [rank],
+        ]
+    };
+
+    start = {
+        "text": start_intro,
+        "options": [
+            [elimination],
+            [death],
+            [survival],
+            [position]
+        ]
+    };
+}
 
 let cache = {}
 
-// semantic relationship
-let elimination = {
-    "type": "elimination",
-    "text": elimination_intro,
-    "options": [
-        [time],
-        [time, count],
-        [count],
-        [spicy_end, time, count]
-    ]
-};
 
-let death = {
-    "type": "death",
-    "text": death_intro,
-    "options": [
-        [time]
-    ]
-};
-
-let position = {
-    "type": "posisition",
-    "text": position_intro,
-    "options": [
-        [filler],
-        [spicy_end, filler]
-    ]
-};
-
-let survival = {
-    "type": "survival",
-    "text": survival_intro,
-    "options": [
-        [rank],
-    ]
-};
-
-let start = {
-    "text": start_intro,
-    "options": [
-        [elimination],
-        [death],
-        [survival],
-        [position]
-    ]
-}
 
 let randomizer = function(list_items) {
     return list_items[Math.floor(Math.random() * Math.floor(list_items.length))];
@@ -74,6 +140,8 @@ function isDict(v) {
 
 // let challenge_options = [start];
 async function challengeSelector(state) {
+
+    generateVocab(state.players);
     let conversation_pipeline = [];
     console.log(cache);
     if (Object.keys(cache).length == 0) {
@@ -94,6 +162,7 @@ async function challengeSelector(state) {
             }
             cache["kills"] = state.kills;
         }
+        generateVocab(state.players);
         if (cache["players"] - state.players > 0) {
             for (var i = 0; i < 3; i = i+1) {
                 conversation_pipeline.push(start_intro);
